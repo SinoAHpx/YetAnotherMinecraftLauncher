0a3fa80cbb7c9c9c25081fb2341de5df4040fd1e
AHpx <ahpx@yandex.com>
2024-06-16 16:06:11 +0800

    Added basic accounting adding logics

--- YetAnotherMinecraftLauncher/ViewModels/AccountsViewModel.cs
+++ YetAnotherMinecraftLauncher/ViewModels/AccountsViewModel.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reactive;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 using ReactiveUI;
 using YetAnotherMinecraftLauncher.Views;
+using YetAnotherMinecraftLauncher.Views.Controls;
 
 namespace YetAnotherMinecraftLauncher.ViewModels
 {
     internal class AccountsViewModel : ViewModelBase
     {
         public ReactiveCommand<Unit, Unit> ReturnActionCommand { get; set; }
+
+        private ObservableCollection<SelectiveItem> _accountsList = [];
+
+        public ObservableCollection<SelectiveItem> AccountsList
+        {
+            get => _accountsList;
+            set => this.RaiseAndSetIfChanged(ref _accountsList, value);
+        }
+
+        public ReactiveCommand<Unit, Unit> AddAccountActionCommand { get; set; }
 
         #region Actual logics
 
@@ -21,11 +36,31 @@ namespace YetAnotherMinecraftLauncher.ViewModels
             MessageBus.Current.SendMessage(nameof(ReturnToHome), nameof(AccountsView));
         }
 
+        public void AddAccount()
+        {
+            //mocking values
+            AccountsList.Add(new SelectiveItem
+            {
+                Avatar = new Bitmap(AssetLoader.Open(
+                    new Uri("avares://YetAnotherMinecraftLauncher/Assets/DefaultAccountAvatar.png"))),
+                Title = $"Steve{AccountsList.Count + 1}",
+                Subtitle = "Online",
+                SelectAction = ReactiveCommand.Create(() => { }),
+                RemoveAction = ReactiveCommand.Create(() => { })
+            });
+            ;
+        }
+
         #endregion
 
         public AccountsViewModel()
         {
+            #region Register commands
+
             ReturnActionCommand = ReactiveCommand.Create(ReturnToHome);
+            AddAccountActionCommand = ReactiveCommand.Create(AddAccount);
+
+            #endregion
         }
     }
 }
