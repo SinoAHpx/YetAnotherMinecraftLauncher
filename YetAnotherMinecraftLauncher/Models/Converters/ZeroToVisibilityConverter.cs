d8c99dcd29b0ec08a91bf4fa2d7ed5c72189395e
AHpx-Lap <ahpx@yandex.com>
2024-07-13 15:49:22 +0800

    Fixed custom minecraft directory visibility issue

--- YetAnotherMinecraftLauncher/Models/Converters/ZeroToVisibilityConverter.cs
+++ YetAnotherMinecraftLauncher/Models/Converters/ZeroToVisibilityConverter.cs
@@ -9,13 +9,32 @@ using Avalonia.Data.Converters;
 
 namespace YetAnotherMinecraftLauncher.Models.Converters
 {
-    internal class ItemCountToVisibilityConverter : IValueConverter
+    internal class ZeroToVisibilityConverter : IValueConverter
     {
         public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
-            if (value is int count)
+            if (value is int i)
             {
-                return count == 0 ? true : false;
+                return i == 0;
+            }
+
+            return new BindingNotification(new InvalidCastException(),
+                BindingErrorType.Error);
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class ZeroToInvisibilityConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is int i)
+            {
+                return i != 0;
             }
 
             return new BindingNotification(new InvalidCastException(),
